06b08e964c59096a3fba7c5d87d4bed816425ab0
Aishwarya Sriram <98.aishu@gmail.com>
2022-02-10 11:33:06 +0530

    removed key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCWqQ2Vu00Kgy1OjWO7wMR6Vhe0ddi6-Ms"
+const KEY = "obtain from https://console.developers.google.com/home/dashboard?project=videos-340903"
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
