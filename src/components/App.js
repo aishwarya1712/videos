bf18cff22d63b9f93a061b499536cd517ff7b375
Aishwarya Sriram <98.aishu@gmail.com>
2022-02-23 10:46:43 +0530

    Refactored App.js to be a functional component instead of class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,100 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SearchBar from "./SearchBar";
 import youtube from '../apis/youtube';
 import VideoList from './VideoList'
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component {
-    constructor(props){
-        super(props);  
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        }
-    }
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
-    componentDidMount(){
-        this.onFormSubmit("jimin");
-    }
-    
-    onFormSubmit = async (term) => {
+    useEffect(()=>{
+        onFormSubmit('jimin')
+    }, [])
+
+    const onFormSubmit = async (term) => {
         console.log("Search term submitted is: ", term)
         const res = await youtube.get('/search', {
             params:{
                 q: term
             }
         })
-        this.setState({
-            videos: res.data.items,
-            selectedVideo: res.data.items[0]
-        },()=>{
-            console.log(this.state.videos)
-        })
+        setVideos(res.data.items);
+        setSelectedVideo(res.data.items[0])
     }
 
-    setSelectedVideo = (video) => {
-        console.log("In set selected video", video)
-        this.setState({
-            selectedVideo: video
-        })
-    }
-
-    render(){
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onFormSubmit}/>
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail selectedVideo={this.state.selectedVideo}/>
-                        </div>
-                        <div className="five wide column">
-                            <VideoList videos={this.state.videos} setSelectedVideo={this.setSelectedVideo}/>
-                        </div>
+    return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={onFormSubmit}/>
+            <div className="ui grid">
+                <div className="ui row">
+                    <div className="eleven wide column">
+                        <VideoDetail selectedVideo={selectedVideo}/>
+                    </div>
+                    <div className="five wide column">
+                        <VideoList videos={videos} setSelectedVideo={setSelectedVideo}/>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default App;
+
+// -------- class component-----------
+// class App extends React.Component {
+//     constructor(props){
+//         super(props);  
+//         this.state = {
+//             videos: [],
+//             selectedVideo: null
+//         }
+//     }
+
+//     componentDidMount(){
+//         this.onFormSubmit("jimin");
+//     }
+    
+//     onFormSubmit = async (term) => {
+//         console.log("Search term submitted is: ", term)
+//         const res = await youtube.get('/search', {
+//             params:{
+//                 q: term
+//             }
+//         })
+//         this.setState({
+//             videos: res.data.items,
+//             selectedVideo: res.data.items[0]
+//         },()=>{
+//             console.log(this.state.videos)
+//         })
+//     }
+
+//     setSelectedVideo = (video) => {
+//         console.log("In set selected video", video)
+//         this.setState({
+//             selectedVideo: video
+//         })
+//     }
+
+//     render(){
+//         return (
+//             <div className="ui container">
+//                 <SearchBar onFormSubmit={this.onFormSubmit}/>
+//                 <div className="ui grid">
+//                     <div className="ui row">
+//                         <div className="eleven wide column">
+//                             <VideoDetail selectedVideo={this.state.selectedVideo}/>
+//                         </div>
+//                         <div className="five wide column">
+//                             <VideoList videos={this.state.videos} setSelectedVideo={this.setSelectedVideo}/>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
