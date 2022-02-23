25a2dfcf57d6f88af7ae79c3c5791fcd0f6da824
Aishwarya Sriram <98.aishu@gmail.com>
2022-02-23 10:34:21 +0530

    Refactored SearchBar.js to be a functional component instead of class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 
-class SearchBar extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state = {
-            term: ""
-        }
-    }
-
-    onInputChange(e) {
-        this.setState({
-            term: e.target.value
-        })  
-    }
-
-    onSearchSubmit = (e) => {
+const SearchBar = ({onFormSubmit}) => {
+    const [term, setTerm] = useState('');
+    
+    const onSearchSubmit = (e) => {
         e.preventDefault();
-        this.props.onFormSubmit(this.state.term);
+        onFormSubmit(term);
     }
 
-    render(){
-        return (
-            <div className="search-bar ui segment">
-                <form className="ui form" onSubmit={this.onSearchSubmit}>
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input type="text" value={this.state.term} onChange={(e) => {this.onInputChange(e)}}/>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className="search-bar ui segment">
+            <form className="ui form" onSubmit={onSearchSubmit}>
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" value={term} onChange={(e) => setTerm(e.target.value)}/>
+                </div>
+            </form>
+        </div>
+    )
+    
 }
 
 export default SearchBar;
+
+
+// ----- Using class components ----------
+// class SearchBar extends React.Component{
+
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             term: ""
+//         }
+//     }
+
+//     onInputChange(e) {
+//         this.setState({
+//             term: e.target.value
+//         })  
+//     }
+
+//     onSearchSubmit = (e) => {
+//         e.preventDefault();
+//         this.props.onFormSubmit(this.state.term);
+//     }
+
+//     render(){
+//         return (
+//             <div className="search-bar ui segment">
+//                 <form className="ui form" onSubmit={this.onSearchSubmit}>
+//                     <div className="field">
+//                         <label>Video Search</label>
+//                         <input type="text" value={this.state.term} onChange={(e) => {this.onInputChange(e)}}/>
+//                     </div>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+// export default SearchBar;
